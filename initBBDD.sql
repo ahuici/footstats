d39f7df6ce60733238b11e950abcb458688a58dc
ahuici <ahuicidelg1@educacion.navarra.es>
2026-02-03 20:43:14 +0100

    Script creacion base de datos

--- initBBDD.sql
+++ initBBDD.sql
@@ -1,7 +1,26 @@
-CREATE DATABASE futbol;
+CREATE DATABASE FUTSTATS;
 
-USE futbol;
+USE FUTSTATS;
 
-CREATE TABLE jugadores (
-    id INT AUTO_INCREMENT PRIMARY KEY
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pwd VARCHAR(200),
+    name VARCHAR(200),
+    surname VARCHAR(200),
+    age INT,
+    gender ENUM('hombre', 'mujer', 'otro'),
+    privilege INT
+);
+
+CREATE TABLE players (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    creator_id INT,
+    name VARCHAR(200),
+    surname VARCHAR(200),
+    number INT,
+    team VARCHAR(200),
+    selection VARCHAR(200),
+    goals INT,
+    position ENUM('portero','defensa','mediocentro','delantero'),
+    FOREIGN KEY (creator_id) REFERENCES users(id)
 );
